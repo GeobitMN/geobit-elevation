c5ec39f34c77ef574f7893a97b782260039f83d8
Mike Nawrocki <majkeliusz@gmail.com>
2023-12-17 21:27:36 +0100

    feat: Retrieve x y z values of a point loaded from file

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
-import sys
-import ezdxf
+from dxf import DXFHandler
 
 
 if __name__ == "__main__":
-    try:
-        doc = ezdxf.readfile("./res/SIATKA_5x5cm_POLILINIE_3DPUNKTY_TEST_1.dxf")
-    except IOError:
-        print(f"Not a DXF file or a generic I/O error.")
-        sys.exit(1)
-    except ezdxf.DXFStructureError:
-        print(f"Invalid or corrupted DXF file.")
-        sys.exit(2)
+    file_location = "./res/SIATKA_5x5cm_POLILINIE_3DPUNKTY_TEST_1.dxf"
+    handler = DXFHandler(file_location=file_location)
 
-    msp = doc.modelspace()
-    print(msp)
-    lines = msp.query("POINT")
-    print(lines)
+    layer = handler.create_new_layer("test_layer")
+    lines = handler.lines
+
+    for line in lines:
+        for point in line.vertices:
+            print(point.x, point.y, point.z)
