05397491dd19a685c0e369c036269849aa685f4b
Mike Nawrocki <majkeliusz@gmail.com>
2023-12-21 07:58:43 +0100

    feat: Remove redundand layer creation

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ if __name__ == "__main__":
     file_location = "./res/SIATKA_5x5cm_POLILINIE_3DPUNKTY_TEST_1.dxf"
 
     handler = DXFHandler(file_location=file_location)
-    layer = handler.create_new_layer("test_layer")
     lines = handler.lines
 
     manager = Manager(
