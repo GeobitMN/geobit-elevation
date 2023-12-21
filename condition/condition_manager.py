72e71a32df807ae73d66d905497f18bfb8807a8e
Mike Nawrocki <majkeliusz@gmail.com>
2023-12-21 07:58:06 +0100

    feat: Save calculated points onto DXF file

--- condition/condition_manager.py
+++ condition/condition_manager.py
@@ -12,7 +12,13 @@ class Manager:
         self.tolerance = condition.tolerance
 
     def run_calculations(self):
-        for line in self.lines:
+        for count, line in enumerate(self.lines, 1):
+            # Get new layer on which we will store heights
+            layer = self.handler.create_new_layer(layer_name=f"#{count} {line.vertices[0].layer_name}")
+
+            # Keep track of calculated points
+            height_points = []
+
             length = len(line.vertices)
             first_index = 0
             second_index = first_index + self.offset  # Gives 2nd point with distance for the given condition
@@ -26,9 +32,15 @@ class Manager:
                 height = calculate_height(a=point_a, b=point_b)
                 delta = height_difference(point=point_mid, height=height)
 
-                print(delta)
+                # Store point
+                height_point =  (point_mid.x, delta, 0)
+                height_points.append(height_point)
 
                 first_index += 1
                 second_index += 1
                 middle_index += 1
+
+            print(height_points)
+            self.handler.store_polyline(layer_name=layer, points=height_points)
+            self.handler.store_points(layer_name=layer, points=height_points)
 
