cd9e101b1c8fadb22d2c00e96b680f10f42d31f2
Mike Nawrocki <majkeliusz@gmail.com>
2024-01-03 05:20:38 +0100

    feat: Add progress bar for the calculations

--- condition/condition_manager.py
+++ condition/condition_manager.py
@@ -1,4 +1,4 @@
-import math
+from alive_progress import alive_bar
 
 from calculations import calculate_height, height_difference
 from condition import Condition
@@ -22,40 +22,44 @@ class Manager:
         return 0 if a == b else 90
 
     def run_calculations(self):
-        for count, line in enumerate(self.lines, 1):
-            # Get new layer on which we will store heights
-            layer = self.handler.create_new_layer(layer_name=f"#{count} {line.vertices[0].layer_name}")
+        with alive_bar(total=len(self.lines), title=f"Przetwarzanie warunku #{self.type}") as bar:
+            for count, line in enumerate(self.lines, 1):
+                # Get new layer on which we will store heights
+                layer = self.handler.create_new_layer(layer_name=f"#{count} {line.vertices[0].layer_name}")
 
-            # Keep track of calculated points
-            height_points = []
+                # Keep track of calculated points
+                height_points = []
 
-            length = len(line.vertices)
-            first_index = 0
-            second_index = first_index + self.offset  # Gives 2nd point with distance for the given condition
-            middle_index = int(self.offset/2)
+                length = len(line.vertices)
+                first_index = 0
+                second_index = first_index + self.offset  # Gives 2nd point with distance for the given condition
+                middle_index = int(self.offset/2)
 
-            while second_index < length:
-                point_a = line[first_index]
-                point_b = line[second_index]
-                point_mid = line[middle_index]
+                while second_index < length:
+                    point_a = line[first_index]
+                    point_b = line[second_index]
+                    point_mid = line[middle_index]
 
-                height = calculate_height(a=point_a, b=point_b)
-                delta = height_difference(point=point_mid, height=height)
+                    height = calculate_height(a=point_a, b=point_b)
+                    delta = height_difference(point=point_mid, height=height)
 
-                # Store point
-                # Point is created as follows:
-                # We use the same X,
-                # Add the delta of height to the Y - to obtain a difference in elevation on plot
-                # And use Delta as Z for safekeeping.
-                height_point = (point_mid.x, point_mid.y + delta, delta)
-                height_points.append(height_point)
+                    # Store point
+                    # Point is created as follows:
+                    # We use the same X,
+                    # Add the delta of height to the Y - to obtain a difference in elevation on plot
+                    # And use Delta as Z for safekeeping.
+                    height_point = (point_mid.x, point_mid.y + delta, delta)
+                    height_points.append(height_point)
 
-                first_index += 1
-                second_index += 1
-                middle_index += 1
+                    first_index += 1
+                    second_index += 1
+                    middle_index += 1
 
-            print(height_points)
-            text_rotation = self.calculate_point_rotation(point_a=point_a, point_b=point_b)
-            self.handler.store_polyline(layer_name=layer, points=height_points)
-            self.handler.store_points(layer_name=layer, points=height_points, text_rotation=text_rotation)
+                if point_a == None or point_b == None:
+                    break
+                text_rotation = self.calculate_point_rotation(point_a=point_a, point_b=point_b)
+                self.handler.store_polyline(layer_name=layer, points=height_points)
+                self.handler.store_points(layer_name=layer, points=height_points, text_rotation=text_rotation)
+                # print(height_points)
+                bar()
 
