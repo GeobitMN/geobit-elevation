1a32418f157df19ab2b7b6cf89ee7fe6a5992f46
Mike Nawrocki <michal.m.nawrocki@gmail.com>
2024-01-04 01:28:51 +0100

    fix: Revert "feat: Add comment"
    This reverts commit 4b56eb577d8472a5a2240b5d3bf6bfb363c5fcd5.

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from dxf import DXFHandler
 
 
 if __name__ == "__main__":
-    # File location
     file_location = "./res/SIATKA_5x5cm_POLILINIE_3DPUNKTY_TEST_1.dxf"
 
     handler = DXFHandler(file_location=file_location)
