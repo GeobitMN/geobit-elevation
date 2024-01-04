4b56eb577d8472a5a2240b5d3bf6bfb363c5fcd5
Mike Nawrocki <michal.m.nawrocki@gmail.com>
2024-01-04 01:28:00 +0100

    feat: Add comment

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from dxf import DXFHandler
 
 
 if __name__ == "__main__":
+    # File location
     file_location = "./res/SIATKA_5x5cm_POLILINIE_3DPUNKTY_TEST_1.dxf"
 
     handler = DXFHandler(file_location=file_location)
